1a530e49dd9c1075b2f6fbc98a70fc08dc39365b
Wiekonek <wiekonek@gmail.com>
2014-10-21 20:11:30 +0200

    + nested class Order (UP instance.cpp, instance.h)

--- instance.cpp
+++ instance.cpp
@@ -7,16 +7,17 @@ using std::fstream;
 using std::cout;
 
 
+// Instance
 
-bool Instance::import_data(char* file_name, int amount)
+bool Instance::import_data(char* file_name, int _amount)
 {
-  int what = 1, index = 0;
+  int what = 1, index = 0, customer_number = 1;
   bool plus = false;
   char c;
   fstream fp (file_name);
-  vector <int> tmp(6,0);
-  K = amount;
- 
+  vector <int> tmp(7,0);
+  amount = _amount;
+  Order *order;
   
   Text *buff = new Text();
 
@@ -41,25 +42,32 @@ bool Instance::import_data(char* file_name, int amount)
     {
       fp.seekg(-1, std::ios_base::cur);
       fp.getline(buff->get_ptr(), 10, '\r');
+      cout<<buff->get_ptr()<<std::endl;
       Q = buff->to_int();
       plus = true;
     }
 
 
-    if(what > 10 && what <= K+10 && c != ' ')
+    if(what > 10 && what <= amount+10 && c != ' ')
     {
       fp.seekg(-1, std::ios_base::cur);
       fp.getline(buff->get_ptr(), 10, ' ');
-
-      if(index++ > 1)
-	tmp[index-3] = buff->to_int();
-
+      
+      if(index++ > 0)
+	  tmp[index - 2] = buff->to_int();
+ 
       if(index == 8)
       {
-	orders.push_back(tmp);
-
+	cout<<tmp.size()<<": ";
+	for(int i = 0; i <= tmp.size() - 1; i++)
+	  cout<<tmp[i]<<"  ";
+	cout<<"\n";
+	order = new Order(tmp);
+	orders.push_back(*order);
+	delete order;
+	
 	index = 0;
-	what++;
+	plus = true;
       }
     }
 
@@ -77,15 +85,64 @@ bool Instance::import_data(char* file_name, int amount)
   return true;
 }
 
+int Instance::nearest(int customer_number)
+{
+  int ncn; //x = orders[customer_number][1]; y = orders[customer_number][2];
+  
+  for(int i = 1; i <= amount; i++) {
+    
+  }
+  
+  
+  return ncn;
+}
+
+
 void Instance::show()
 {
   cout<<"Instance: \nK = "<< K <<"\nQ = "<< Q << "\n";
+  cout<<"CUS NUM\tX\tY\tDEMAND\tREADY T\tDUE DATE\tSER DURAT\n";
   for(int i = 0; i < orders.size(); i++)
-  {
-    cout<<i<<":\t";
-    for(int j = 0; j < orders[i].size(); j++)
-      cout<<orders[i][j]<<"\t";
-    cout<<"\n";
-  }
+    cout<<orders[i].get_i()<<"\t"<<orders[i].get_x()<<"\t"<<orders[i].get_y()<<"\t"<<orders[i].get_q()<<"\t"<<orders[i].get_e()<<"\t"<<orders[i].get_l()<<"\t"<<orders[i].get_d()<<"\n";
 }
+
+
+
+
+
+
+//           ORDER
+
+Instance::Order::Order(vector< int > vec): i(vec[0]), x(vec[1]), y(vec[2]), q(vec[3]), e(vec[4]), l(vec[5]), d(vec[6]) {
+}
+
+int Instance::Order::get_i() {
+    return i;
+}
+
+int Instance::Order::get_x() {
+    return x;
+}
+
+int Instance::Order::get_y() {
+    return y;
+}
+
+int Instance::Order::get_q() {
+    return q;
+}
+
+int Instance::Order::get_e() {
+    return e;
+}
+
+int Instance::Order::get_l() {
+    return l;
+}
+
+int Instance::Order::get_d() {
+    return d;
+}
+
+
 
--- instance.h
+++ instance.h
@@ -10,11 +10,28 @@ using std::vector;
 class Instance
 {
 private:
-  int K, Q;
-  vector< vector<int> > orders;
+  int K, Q, amount;
+  
+  class Order
+  {
+  private:
+    int i, x, y, q, e, l, d;
+  public:
+    Order(vector<int> ord_vec);
+    int get_i();
+    int get_x();
+    int get_y();
+    int get_q();
+    int get_e();
+    int get_l();
+    int get_d();
+  };
+  
+  vector<Order> orders;
 public:
-  bool import_data(char* file_name, int amount);
-  void show();
+  bool import_data(char* file_name, int _amount); //import data from file 
+  int nearest(int customer_number); //return nearest customer number
+  void show(); //show data in object
 };
 
 #endif // INSTANCE_H
